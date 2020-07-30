b0639943101747c2f587d7d67e256d78ccf3be85
Rahul Kumar <undefined.rahulraj@gmail.com>
2020-07-30 17:26:31 +0530

    Create call_me.js

---
+++ call_me.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('demo').innerHTML = "<h2>Please enter the details required for Checkout...[This is from GitHub]</h2>"; 
+});
